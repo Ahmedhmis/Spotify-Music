58b816d002eaef349a33922f04b1e54cf1072926
Ahmedhmis <96076993+Ahmedhmis@users.noreply.github.com>
2022-01-08 18:58:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 from pytgcalls import idle
-from callsmusic import run
+from callmusic import run
 from Spotify import __version__
 from pyrogram import Client as Bot
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
